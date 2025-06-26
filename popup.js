f8395810d5813d6fe13fdba7c489f8729a762a8b
H.O.S <gawadkh319@hotmail.com>
2025-06-26 10:00:40 +0300

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
